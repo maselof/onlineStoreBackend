330b3264b8480bcf15bc49093a2427e4c7a3e413
maselof <mr.gindeev@gmail.com>
2024-03-09 16:54:06 +0300

    fix-redis

--- api/controllers/carts.go
+++ api/controllers/carts.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"onlineStoreBackend/constants"
 	"onlineStoreBackend/entity/request"
@@ -40,6 +41,7 @@ func (s CartsController) PostProductsByUserID(c *gin.Context) {
 	}
 
 	err = s.cartsService.PostProducts(c, requestCast)
+	log.Print(err)
 	if err != nil {
 		resp.SendResponse(c, response.JSONResponse{
 			Code:    http.StatusInternalServerError,
